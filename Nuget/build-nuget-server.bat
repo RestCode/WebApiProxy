38be8acacdcfee962f9671061536808434b7a2eb
faniereynders <faniereynders@gmail.com>
2014-01-23 15:54:18 +0200

    Added 'webapiproxy' codesnippet to C# client generator

--- Nuget/build-nuget-server.bat
+++ Nuget/build-nuget-server.bat
@@ -3,7 +3,7 @@ set path=.
 if not [%1]==[] set path=%1
 
 echo Creating WebProxyApi Package
- %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.nuspec -Version 1.0.0.0 -OutputDirectory %path%\..\Nuget\Packages
+ %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.nuspec -Version 1.0.1.0 -OutputDirectory %path%\..\Nuget\Packages
 
 if errorlevel 1 echo Error creating WebApiProxy Package
 
--- WebApiProxy.Sample.Client/Program.cs
+++ WebApiProxy.Sample.Client/Program.cs
@@ -6,9 +6,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using ProxyApi.Proxies.Models;
+using System.Threading;
+using System.Net.Http.Formatting;
 
 namespace WebApiProxy.Sample.Client
 {
+
+    //public static class MyClass
+    //{
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message)
+    //    {
+    //        return message.Content.ReadAsAsync<T>();
+    //    }
+
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message, CancellationToken cancellationToken)
+    //    {
+    //        return message.Content.ReadAsAsync<T>(cancellationToken);
+    //    }
+
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message, IEnumerable<MediaTypeFormatter> formatters)
+    //    {
+    //        return message.Content.ReadAsAsync<T>(formatters);
+    //    }
+
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message, IEnumerable<MediaTypeFormatter> formatters, CancellationToken cancellationToken)
+    //    {
+    //        return message.Content.ReadAsAsync<T>(formatters,cancellationToken);
+    //    }
+
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message, IEnumerable<MediaTypeFormatter> formatters, IFormatterLogger formatterLogger)
+    //    {
+    //        return message.Content.ReadAsAsync<T>(formatters, formatterLogger);
+    //    }
+
+    //    public static Task<T> ContentAsAsync<T>(this HttpResponseMessage message, IEnumerable<MediaTypeFormatter> formatters, IFormatterLogger formatterLogger, CancellationToken cancellationToken)
+    //    {
+    //        return message.Content.ReadAsAsync<T>(formatters, formatterLogger,cancellationToken);
+    //    }
+    //}
     class Program
     {
         static void Main(string[] args)
@@ -19,15 +54,30 @@ namespace WebApiProxy.Sample.Client
 
         static async void Get()
         {
-            using (var client = new PeopleClient())
+            
+            using (var client = new ApiClient())
             {
-
-                client.GetAsync("sddd", 22, "aaa");
-
-                var response = await client.PostAsync(new Person { Id = 2, FirstName = "SSSS" });
-
-                //var people = await response.Content.ReadAsAsync<Person[]>();
+                var response = await client.GetAsync( );
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsAsync<object>();
             }
+	    
+        }
+	    
+
+
+
+
+
+            
+          
+	    
+	    
+	    
+
+            
+	    
+	    
         }
     }
 }
