a2d46b5a80d8abef17b5eea1df7cd9ebf1542426
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-08-19 17:00:54 -0300

    Server - config nuget package

--- Nuget/build-nuget-server.bat
+++ Nuget/build-nuget-server.bat
@@ -3,7 +3,8 @@ set path=.
 if not [%1]==[] set path=%1
 
 echo Creating WebProxyApi Package
- %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.nuspec -Version 1.0.1.0 -OutputDirectory %path%\..\Nuget\Packages
+ %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.Server.csproj -IncludeReferencedProjects -OutputDirectory %path%\..\Nuget\Packages
 
 if errorlevel 1 echo Error creating WebApiProxy Package
 
+pause;
--- WebApiProxy.Server/Properties/AssemblyInfo.cs
+++ WebApiProxy.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApiProxy.Server")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WebApiProxy.Server-Bardock")]
+[assembly: AssemblyDescription("Provides an endpoint for ASP.NET Web Api services to serve a JavaScript proxy and metadata")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApiProxy.Server")]
+[assembly: AssemblyCompany("bardock")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
