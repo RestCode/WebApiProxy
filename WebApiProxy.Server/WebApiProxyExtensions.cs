32ff94b63909c212a6d803926afe4bff5c2cf452
Fanie Reynders <fanie@reynders.co>
2015-01-22 06:29:49 +0100

    Made explicit proxy route registration opt-in as extension method extending HttpConfiguration

---
+++ WebApiProxy.Server/WebApiProxyExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using WebApiProxy.Server;
+
+namespace WebApiProxy.Server
+{
+    public static class WebApiProxyExtensions
+    {
+        /// <summary>
+        /// Sets up the proxy route table entries.
+        /// </summary>
+        public static void RegisterProxyRoutes(this HttpConfiguration config, string routeTemplate = "api/proxies")
+        {
+            config.Routes.MapHttpRoute(
+            name: "WebApiProxy",
+            routeTemplate: routeTemplate,
+            defaults: new { id = RouteParameter.Optional },
+            constraints: null,
+            handler: new ProxyHandler(config) { InnerHandler = new HttpControllerDispatcher(config) }
+        );
+
+
+        }
+    }
+}
