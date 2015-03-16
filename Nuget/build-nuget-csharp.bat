1a8c1e8bf31dfa60358cce9737144d2695fe821a
Matt Melton <matthew.melton@profilegroup.co.uk>
2015-03-16 16:50:16 +0000

    Update build nuspec and build files
    Use $configuration$, create Packages folder if it doesn't exist. Bundle
    lib in lib\net45 etc.

--- Nuget/build-nuget-csharp.bat
+++ Nuget/build-nuget-csharp.bat
@@ -2,8 +2,10 @@
 set path=.
 if not [%1]==[] set path=%1
 
+IF NOT EXIST Packages MKDIR Packages
+
 echo Creating WebProxyApi CSharp Package
- %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Tasks\WebApiProxy.Tasks.csproj -IncludeReferencedProjects -OutputDirectory %path%\Packages
+ %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Tasks\WebApiProxy.Tasks.csproj -IncludeReferencedProjects -OutputDirectory %path%\Packages -Properties Configuration=Release -Verbose 
 
 if errorlevel 1 echo Error creating WebApiProxy CSharp Package
 
--- Nuget/build-nuget-server.bat
+++ Nuget/build-nuget-server.bat
@@ -2,8 +2,10 @@
 set path=.
 if not [%1]==[] set path=%1
 
+IF NOT EXIST Packages MKDIR Packages
+
 echo Creating WebProxyApi Package
- %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.Server.csproj -IncludeReferencedProjects -OutputDirectory %path%\..\Nuget\Packages
+ %path%\..\.nuget\nuget pack %path%\..\WebApiProxy.Server\WebApiProxy.Server.csproj -IncludeReferencedProjects -OutputDirectory %path%\..\Nuget\Packages -Properties Configuration=Release -Verbose 
 
 if errorlevel 1 echo Error creating WebApiProxy Package
 
