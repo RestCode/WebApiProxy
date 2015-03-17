9140f82afc5f3f971ac26706c3846c935f75762a
Fanie Reynders <fanie@reynders.co>
2015-03-17 04:10:25 +0100

    Added HttpResponseMessage extension for serializing Http content as typed object. Also updated snippet.

---
+++ WebApiProxy.Tasks/Infrastructure/HttpResponseMessageExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+public static class HttpResponseMessageExtensions
+{
+    public static Task<T> ReadContentAsAsync<T>(this HttpResponseMessage response)
+    {
+        return response.Content.ReadAsAsync<T>();
+    }
+}
+
