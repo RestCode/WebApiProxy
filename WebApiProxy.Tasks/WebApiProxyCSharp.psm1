8576bc091fef7bb330865d2664d655a84803af93
$+j <lust4life.jun@gmail.com>
2015-08-05 18:27:22 +0800

    add type from job

--- WebApiProxy.Tasks/WebApiProxyCSharp.psm1
+++ WebApiProxy.Tasks/WebApiProxyCSharp.psm1
@@ -1,32 +1,35 @@
 ﻿function WebApiProxy-Generate-CSharp() {
+
 	$project = Get-Project
-    
     $projectPath = [System.IO.Path]::GetDirectoryName($project.FullName)
-
 	$root = (Join-Path $projectPath "WebApiProxy\")
-
 	$rootSpaces = "$root"
 
-	$config = [WebApiProxy.Tasks.Models.Configuration]::Load($rootSpaces);
+	$generateJob = Start-Job -ScriptBlock { 
+        param($project,$projectPath,$rootSpace) 
 
-    $generator = New-Object WebApiProxy.Tasks.Infrastructure.CSharpGenerator -ArgumentList @($config)
-    
-    
-    
-    $fileName = (Join-Path $projectPath "WebApiProxy\WebApiProxy.generated.cs")
-    
-	Write-Host "Generating proxy code..."
+		Add-Type -Path (Join-Path $projectPath "bin\Debug\WebApiProxy.Tasks.dll")
+		$config = [WebApiProxy.Tasks.Models.Configuration]::Load($rootSpaces);
 
-    $source = $generator.Generate()
+		$generator = New-Object WebApiProxy.Tasks.Infrastructure.CSharpGenerator -ArgumentList @($config)
+		$fileName = (Join-Path $projectPath "WebApiProxy\WebApiProxy.generated.cs")
     
-    $result = New-Item $fileName `
-          -ItemType "file" -Force `
-          -Value $source
+		Write-Host "Generating proxy code..."
+
+		$source = $generator.Generate()
     
-    $item = $project.ProjectItems.AddFromFile($fileName)
-
-	Write-Host "Done."
-
-}
+		$result = New-Item $fileName `
+			  -ItemType "file" -Force `
+			  -Value $source
+    
+		$item = $project.ProjectItems.AddFromFile($fileName)
+		$source
+		Write-Host "Done..."
+	 } -ArgumentList @($project,$projectPath,$rootSpace)
+	 
+	 $result = Receive-Job -Job $generateJob -Wait
+	 Write-Host $result
+     Write-Host "Done."
+} 
 
 Export-ModuleMember "WebApiProxy-Generate-CSharp"
