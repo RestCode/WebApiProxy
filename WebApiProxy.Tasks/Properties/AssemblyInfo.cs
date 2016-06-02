61a4e2118716111c1b9d06bcc14fbe07f7574bdc
$+j <lust4life.jun@gmail.com>
2016-06-02 10:43:59 +0800

    use origin names convention

--- WebApiProxy.Tasks/Properties/AssemblyInfo.cs
+++ WebApiProxy.Tasks/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WebApiProxy C# Proxy Client Generator")]
 [assembly: AssemblyDescription("Generates a C# proxy for communicating to a RESTful Web Api that implements the WebApiProxy provider")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("UOKO.COM")]
+[assembly: AssemblyCompany("WebApiProxy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.5.*")]
-[assembly: AssemblyVersion("1.5.*")]
-[assembly: AssemblyFileVersion("1.5.*")]
+[assembly: AssemblyInformationalVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
