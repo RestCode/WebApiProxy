05cf008b8ff29029777e41b962ed6cefa0f3ebe5
$+j <lust4life.jun@gmail.com>
2016-06-06 21:11:03 +0800

    add consul check

--- WebApiProxy.Tasks/WebApiProxyCSharp.psm1
+++ WebApiProxy.Tasks/WebApiProxyCSharp.psm1
@@ -1,7 +1,7 @@
-﻿function WebApiProxy-Generate-CSharp() {
+﻿function WebApiProxy-Generate-CSharp($endpoint) {
 
 	$project = Get-Project
-    	$projectPath = [System.IO.Path]::GetDirectoryName($project.FullName)
+	$projectPath = [System.IO.Path]::GetDirectoryName($project.FullName)
 	$root = (Join-Path $projectPath "WebApiProxy\")
 	$rootSpaces = "$root"
 	$taskPath = ($project.Object.References | where {$_.Identity -eq 'WebApiProxy.Tasks'} | Select-Object -first 1).Path
@@ -14,6 +14,9 @@
 
 
 		$config = [WebApiProxy.Tasks.Models.Configuration]::Load($rootSpaces);
+        if ($endpoint){
+            $config.Endpoint = $endpoint 
+        }
 
 		$generator = New-Object WebApiProxy.Tasks.Infrastructure.CSharpGenerator -ArgumentList @($config)
 		$fileName = (Join-Path $projectPath "WebApiProxy\WebApiProxy.generated.cs")
@@ -34,4 +37,41 @@
     Write-Host "Done."
 } 
 
-Export-ModuleMember "WebApiProxy-Generate-CSharp"
+function ApiProxy-Generate($serviceName)
+{
+    $consulHost = "http://localhost:8500"
+    $traefikInfo = iwr -Uri "${consulHost}/v1/health/service/traefik?passing" | ConvertFrom-Json
+    
+    $endpoint = getAddress -infos $traefikInfo -serviceName "traefik" 
+    
+    if (-not $endpoint){
+        $serviceInfo = iwr -Uri "${consulHost}/v1/health/service/${serviceName}" | ConvertFrom-Json
+        $endpoint = getAddress -infos $serviceInfo -serviceName $serviceName
+    }
+
+    $endpoint = "http://$endpoint/api/proxies/"
+    Write-Host "generate from $endpoint"
+	WebApiProxy-Generate-CSharp $endpoint
+}
+
+function getAddress($infos,$serviceName){
+    $address = ""
+    foreach ($info in $infos)
+    {
+        $isCheckOk = $false
+        foreach($check in $info.Checks)
+        {
+            if($check.Name -eq $serviceName -and $check.Status -eq "passing")
+            {
+                $isCheckOk = $true
+            }
+        }
+
+        if($isCheckOk){
+            $address = "$($info.Service.Address):$($info.Service.Port)"
+        }
+    }
+    $address
+}
+
+Export-ModuleMember "WebApiProxy-Generate-CSharp","ApiProxy-Generate"
